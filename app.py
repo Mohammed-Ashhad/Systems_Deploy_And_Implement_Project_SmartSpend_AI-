198244159daf686ab5998efe0b3eb2fd419d55b2
USA1555 <60098476@udst.edu.qa>
2026-04-05 17:33:27 +0300

    Usama Bin Muslehuddin - v3 submission

--- app.py
+++ app.py
@@ -6,19 +6,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-client = MongoClient(mongo_uri)
-app.config["DB"] = client["smartspend_db"]
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+client = MongoClient(MONGO_URI)
+db = client["smartspend_db"]
+
+# Store db inside app config
+app.config["DB"] = db
 
 from routes.transactions import transactions_bp
-from routes.ai_analysis import ai_bp
-
 app.register_blueprint(transactions_bp)
+
+from routes.ai_analysis import ai_bp
 app.register_blueprint(ai_bp)
 
 @app.route("/health")
 def health():
-    return {"status": "ok", "service": "smartspend-ai"}, 200
+    return {"status": "ok"}, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
